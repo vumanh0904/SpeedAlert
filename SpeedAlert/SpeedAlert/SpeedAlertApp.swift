ecda6a95e054820dd5319c7d0cc29fe0586874d0
imap Mac <goongimap@icloud.com>
2026-03-12 11:39:08 +0700

    initial commit

---
+++ SpeedAlert/SpeedAlert/SpeedAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedAlertApp.swift
+//  SpeedAlert
+//
+//  Created by macos on 12/3/26.
+//
+
+import SwiftUI
+
+@main
+struct SpeedAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
